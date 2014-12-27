40acfd26d6db63da0b8a205e047e95c4e13e9ab8
Diogo <2110319@my.ipleiria.pt>
2014-12-27 17:51:05 +0000

    DataAccess com DBHandler

---
+++ AppWebServiceTest/DataAccess/DBHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+using System.Threading.Tasks;
+using AppWebServiceTest;
+
+namespace DataAccess
+{
+    public class DBHandler
+    {
+        Model1Container model = new Model1Container();
+
+        public Boolean Login(String username, String password)
+        {
+            Boolean isLogged = false;
+
+            if (model.UtilizadorSet.Where(i => i.username == username).First())
+            {
+                if(model.UtilizadorSet.Where(i => i.password == password)){
+                    isLogged = true;
+                }
+            }
+            return isLogged;
+        }
+
+        public List<Utilizador> getListaUtilizadores()
+        {
+            List<Utilizador> listaUtilizadores = new List<Utilizador>();
+            listaUtilizadores = model.UtilizadorSet.ToList();
+            return listaUtilizadores;
+        }
+    }
+}
